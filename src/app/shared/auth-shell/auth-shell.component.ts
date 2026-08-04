d72630440167103154db997fe1191fed9d4c16b3
William White <nilen234@gmail.com>
2026-08-04 17:48:33 -0400

    Signup confirmation text

--- src/app/shared/auth-shell/auth-shell.component.ts
+++ src/app/shared/auth-shell/auth-shell.component.ts
@@ -17,6 +17,7 @@ export class AuthShellComponent {
   password = '';
   accountMode: 'create' | 'existing' = 'existing';
   usePasswordless = false;
+  confirmationText = '';
 
   get isLoggedIn(): boolean {
     return this.userService.isUserLoggedIn();
@@ -36,28 +37,29 @@ export class AuthShellComponent {
     this.password = '';
     this.accountMode = 'existing';
     this.usePasswordless = false;
+    this.confirmationText = '';
   }
 
   submitLogin(): void {
     if (this.usePasswordless) {
       if (this.username.trim()) {
         this.userService.setUserLoggedIn(true);
-        this.closeLoginDialog();
       }
+      this.confirmationText = 'We have sent you an email. Open it and click the Login image to finish logging in.';
       return;
     }
 
     //Creating an account?
     if (this.accountMode === 'create') {
-      console.log(this.username);
-      console.log(this.password);
       this.userService.createNewUser(this.username, this.password);
+      //TODO: Show different text based on if the user already existed
+      this.confirmationText = 'We have sent you an email. Open it and click the Confirm Email image to activate your account.';
     }
+  }
     /*if (this.username.trim() && this.password.trim()) {
       //WW Do this after logging in
       //this.userService.setUserLoggedIn(true);
       //this.closeLoginDialog();
       console.log(this.accountMode);
     }*/
-  }
 }
